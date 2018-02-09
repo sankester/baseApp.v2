bbc76484d5ba8e1a4bf5563e97dae8e574f98363
Ach. Vani Ardiansyah <sankesterfire@gmail.com>
2018-02-09 22:02:22 +0700

    crud portal

---
+++ resources/views/manage/portal/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.base.app')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Portal
+        </h1>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-dashboard"></i> Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Manage</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('manage.portal.index') }}">Portal</a></li>
+            <li class="breadcrumb-item active">Tambah Data</li>
+        </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xl-12 col-md-12 col-lg-12">
+                <!-- Horizontal Form -->
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">
+                            Form Tambah Portal
+                        </h3>
+                        <div class="box-controls pull-right">
+                            <a class="btn btn-sm btn-default" href="{{ route('manage.portal.index') }}"><i class="mdi mdi-chevron-left mr-5"></i> Kembali</a>
+                        </div>
+                    </div>
+                    {{--include notification--}}
+                    @include('layouts.notification.base_notification')
+                    {{--end include notification--}}
+                    <!-- form start -->
+                    {!! Form::model($portal = new \App\Model\Manage\Portal(),['route' => 'manage.portal.store', 'class' => 'form-horizontal form-element','enctype'=>"multipart/form-data", 'id' => 'ubah-user']) !!}
+                        @include('manage.portal.form',['textButton' => 'Simpan', 'icon' => 'mdi mdi-plus'])
+                    {!! Form::close() !!}
+                </div>
+                <!-- /.box -->
+            </div>
+        </div>
+    </section>
+@endsection
+@section('custom-js')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // Default file input style
+            $(".file-styled").uniform({
+                fileButtonClass: 'action btn btn-primary'
+            });
+        });
+    </script>
+@endsection
---
+++ resources/views/manage/portal/form.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: achva
+ * Date: 09/02/2018
+ * Time: 18:07
+ */
